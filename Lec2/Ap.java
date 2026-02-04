e4c79d734e33f627c5180b1c35a430a708842e93
Tenzing lama <dawa7636@gmail.com>
2026-02-04 10:38:48 +0530

    AP problem

---
+++ Lec2/Ap.java
@@ -0,0 +1,13 @@
+package Lec2;
+
+public class Ap {
+    public static void main(String[] args) {
+       int a = 1;
+       int b = 2;
+
+       while (a<=50) {
+        System.out.println(a);
+        a= a+b;
+       }
+    }
+}

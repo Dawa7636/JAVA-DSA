0176c82dfc349d66c3d13e509683f3fa1729cb75
Tenzing lama <dawa7636@gmail.com>
2026-02-05 11:19:25 +0530

    patterns

--- Lec3/pattern12.java
+++ Lec3/pattern12.java
@@ -2,20 +2,22 @@ package Lec3;
 
 public class pattern12 {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 4;
+        int n1 = 5;
         int i = 0;
         while (i<=n) {
             int j =0;
-            while (j<=n) {
-                if (i==0||j==0||i==n-1||j==n-1) {
+            while (j<=n1) {
+                if (i==0||j==0||i==n||j==n1) {
                     System.out.print("* ");
                 }
                 else{
-                    System.out.println();
+                    System.out.print("  ");
                 }
                 j++;
                 
             }
+            System.out.println();
             i++;
         }
     }
--- Lec3/pattern13.java
+++ Lec3/pattern13.java
@@ -1,17 +1,20 @@
 package Lec3;
 
 public class pattern13 {
-    int n1 = 4;
-    int n2 = 5;
-    for(int i=0; i<=n1; i++){
-        for(int j = 0; j<=n2; j++){
-            if (i==0||j==0||i==n1||j==n2) {
-                System.out.print("* ");
+
+    public static void main(String[] args) {
+        int n1=4;
+        int n = 5;
+        for(int i=0; i<=n1; i++){
+            for(int j = 0; j<=n; j++){
+                if (i==0||j==0||i==n1||j==n) {
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.print("  ");
+                }
             }
-            else{
-                System.out.println();
-                
-            }
+            System.out.println();
         }
     }
 }

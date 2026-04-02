d762bdb7ffc2437c25055670456dbd9262bc40c2
Tenzing lama <dawa7636@gmail.com>
2026-04-02 09:47:05 +0530

    array

--- Array/arrayReverse.java
+++ Array/arrayReverse.java
@@ -12,6 +12,8 @@ public class arrayReverse {
             start ++;
             end --;
         }
-        System.out.println(arr[]);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
